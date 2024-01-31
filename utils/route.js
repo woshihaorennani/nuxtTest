2654fc19dd2f962aef6a9801a026d47e8448b761
sun <hongwei.sun@eosi.com.cn>
2024-01-31 12:43:39 +0800

    去掉版本限制

---
+++ utils/route.js
@@ -0,0 +1,9 @@
+
+                export function ReturnIgnoreData() {
+                    return []
+                }
+                export function ReturnRenderData() {
+                    let pageArr = ["/insurance-products","/customer-service","/information-disclosure","/about-us","/insurance-products/health","/insurance-products/family","/insurance-products/future","/customer-service/services","/customer-service/value-added-service","/customer-service/customer-service-dynamics","/customer-service/consumer-rights-protection","/customer-service/download-center","/customer-service/contact-us","/customer-service/ethics-fraud-helpline","/information-disclosure/public-information-disclosure","/information-disclosure/dmtm","/information-disclosure/investment-information-disclosure","/information-disclosure/consultant-marketing-information-disclosure","/information-disclosure/personal-old-age-pension","/about-us/company-info","/about-us/news-center","/about-us/corporate-responsibility","/about-us/business-units","/about-us/career","/about-us/blogs","/about-us/our-brand","/information-disclosure/personal-old-age-pension/aaaeee/qqq12","/insurance-products/health/duhuiankang","/insurance-products/family/zhenyouzhongshen","/insurance-products/future/guanaiyinian","/customer-service/services/commitment","/customer-service/services/claims","/customer-service/services/group-insurance-service","/gsvp","/privacy-policy","/copyright-notice"]
+                    return pageArr
+                }
+            
